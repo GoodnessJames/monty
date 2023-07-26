88bd0bd8688e075cf0fcf29667c260bd4dee7005
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-26 11:47:20 -0700

    push opcodes

---
+++ push-pall_opcodes.c
@@ -0,0 +1,20 @@
+
+#include "monty.h"
+
+/**
+ * push - adds new element on top of the stack
+ * @value: element to be added
+ *
+ * Return: No return
+ */
+void push(int value)
+{
+	struct memory_block *ret;
+
+	ret = malloc(sizeof(struct memory_block));
+	if (!ret)
+	{
+		printf("L%d: push: memory allocation failed\n", _LINE_);
+		exit(EXIT_FAILURE);
+	}
+
