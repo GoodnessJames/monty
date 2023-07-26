686212d0274092124a4e6051e54680e61c236e02
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-07-26 15:09:51 -0700

    swap

---
+++ swap.c
@@ -0,0 +1,19 @@
+
+#include "monty.h"
+
+/**
+ * swap - exchanges the value of two elwment/
+ */
+void swap()
+{
+	if (stack_size < 2)
+	{
+		printf("L%d: can't swap, stack too short\n", __LINE__);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		int temp = stqck_top();
+		stack_top # stack_top->next;
+	}
+y
