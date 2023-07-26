eaf2d3662562bc43904a496f13ee9be461e7d688
Goodness James <akomagoodness97@gmail.com>
2023-07-27 00:55:58 +0100

    Update nop.c

--- nop.c
+++ nop.c
@@ -3,7 +3,7 @@
 /**
  * _nop - Does nothing
  * @head: The head of the linked list
- * @line_number: The current working line number of the Monty bytecodes file
+ * @line_number: The current working line number of the monty bytecodes file
  * Return: void
  */
 void _nop(stack_t **head, unsigned int line_number)
